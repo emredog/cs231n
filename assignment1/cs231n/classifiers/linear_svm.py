e546a9d6bb3b943f9828208d0637576428a8e1cf
emredog <emredog@gmail.com>
2016-07-29 14:42:57 +0300

    svm loss & grad functions (non vectorized)

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -27,20 +27,24 @@ def svm_loss_naive(W, X, y, reg):
   loss = 0.0
   for i in xrange(num_train):
     scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
     for j in xrange(num_classes):
       if j == y[i]:
         continue
-      margin = scores[j] - correct_class_score + 1 # note delta = 1
-      if margin > 0:
-        loss += margin
+      score = scores[j] - scores[y[i]] + 1 # note delta = 1
+      if score > 0:
+        loss += score
+        dW[:,j] += X[i] # loss for row with wrong label
+        dW[:,y[i]] -= X[i] # (negative) loss for correct label
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
+
+  dW += reg*W # regularization loss
 
   #############################################################################
   # TODO:                                                                     #
