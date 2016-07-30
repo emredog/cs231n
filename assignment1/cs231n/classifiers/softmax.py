d81a721c68483ef42ff3afa34adce3372d2656c4
emredog <emredog@gmail.com>
2016-07-30 17:34:35 +0300

    Naive version of softmax classifier is implemented (with loss & grad)

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,45 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
+
+  scores = X.dot(W)
+  
+
+  lossTraining = 0.0
+
+
+  for i in xrange(num_train): # for each sample
+    scores[i,:] += -np.max(scores[i,:]) # trick to keep numerical stability  
+
+    # calculate e^f / Sum_c(e^f_c)    , where c is all classes
+    normExpScores = np.exp(scores[i,:]) / np.sum(np.exp(scores[i,:]))
+
+    # loss for i^th sample is -log(e^f_yi / Sum_c(e^f_c))
+    # so we just need the correct index of normExpScores 
+    lossTraining += -np.log(normExpScores[y[i]])
+
+
+    # analytic gradient is just the multiplication of i'th input and e^f / Sum_c(e^f_c)
+    # so it boils down to (e^f / Sum_c(e^f_c)) * X[i, :]
+    # where shapes are (10 x 1) * (1x3073) = (10 x 3073)
+    # and this signifies the contribution of i'th sample to parameter update
+    dscore = np.reshape(normExpScores, (num_classes, 1)) * X[i, :]
+
+    # we also have to add the d(-f_yi)/dW to the i'th row (equivalent form of loss function)
+    # which is simply X[i,:]
+    dscore[y[i],:] -=  X[i,:]
+
+    dW += dscore.T # W.shape = (3073x10), so transpose dscore to fit it
+
+    
+  lossTraining /= scores.shape[0]
+  lossRegularization = 0.5*reg*np.sum(W*W)
+
+  dW = dW /num_train + reg * W
+
+  loss = lossTraining + lossRegularization
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
